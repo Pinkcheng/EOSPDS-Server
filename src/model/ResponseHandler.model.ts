ef8c42903f6a99b61e7ac7ccb0ce9908400de275
Guantou <guantou.lee@gmail.com>
2021-03-10 15:02:06 +0800

    增加add porter response code

--- src/model/ResponseHandler.model.ts
+++ src/model/ResponseHandler.model.ts
@@ -1,8 +1,8 @@
-import { ADD_PORTER_RESPONSE_STATUS } from '../ResponseCode';
+import { ADD_PORTER_RESPONSE_STATUS } from './ResponseCode';
 
 export class ResponseHandler {
   static set(code: ADD_PORTER_RESPONSE_STATUS, data: Array<any> = []) {
-    const { SUCCESS, ERROR_REPEAT_NAME, ERROR_REPEAT_ACCOUNT, ERROR_REPEAT_TAG_NUMBER, ERROR_NUKNOWN } = ADD_PORTER_RESPONSE_STATUS;
+    const { SUCCESS, WARNING_ID_IS_EMPTY, WARNING_TYPE_IS_EMPTY, WARNING_ACCOUNT_IS_EMPTY, WARNING_NAME_IS_EMPTY, WARNING_PASSWORD_IS_EMPTY, ERROR_REPEAT_NAME, ERROR_REPEAT_ACCOUNT, ERROR_REPEAT_TAG_NUMBER, ERROR_NUKNOWN } = ADD_PORTER_RESPONSE_STATUS;
 
     let message = '';
 
@@ -10,17 +10,32 @@ export class ResponseHandler {
       case SUCCESS:
         message = '傳送員資料新增成功';
         break;
+      case WARNING_NAME_IS_EMPTY:
+        message = '【警告：新增傳送員】傳送員姓名為空';
+        break;
+      case WARNING_ID_IS_EMPTY:
+        message = '【警告：新增傳送員】傳送員編號為空';
+        break;
+      case WARNING_ACCOUNT_IS_EMPTY:
+        message = '【警告：新增傳送員】傳送員帳號為空';
+        break;
+      case WARNING_PASSWORD_IS_EMPTY:
+        message = '【警告：新增傳送員】傳送員密碼為空';
+        break;
+      case WARNING_TYPE_IS_EMPTY:
+        message = '【警告：新增傳送員】傳送員類型為空';
+        break;
       case ERROR_REPEAT_NAME:
-        message = '傳送員資料新增失敗，重複的傳送員姓名';
+        message = '【錯誤：新增傳送員】重複的傳送員姓名';
         break;
       case ERROR_REPEAT_ACCOUNT:
-        message = '傳送員資料新增失敗，重複的傳送員帳號';
+        message = '【錯誤：新增傳送員】重複的傳送員帳號';
         break;
       case ERROR_REPEAT_TAG_NUMBER:
-        message = '傳送員資料新增失敗，重複的標籤編號';
+        message = '【錯誤：新增傳送員】重複的標籤編號';
         break;
       case ERROR_NUKNOWN:
-        message = '傳送員資料新增失敗，發生非預期的錯誤';
+        message = '【錯誤：新增傳送員】發生非預期的錯誤';
         break;
       default:
         throw (new Error('No have response message or code'));
