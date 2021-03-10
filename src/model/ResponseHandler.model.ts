4f2152ccc2350cada75c1ad1ced694fc97e969ab
Guantou <guantou.lee@gmail.com>
2021-03-10 21:40:48 +0800

    增加新增傳送員的response code

--- src/model/ResponseHandler.model.ts
+++ src/model/ResponseHandler.model.ts
@@ -2,7 +2,7 @@ import { ADD_PORTER_RESPONSE_STATUS } from './ResponseCode';
 
 export class ResponseHandler {
   static set(code: ADD_PORTER_RESPONSE_STATUS, data: Array<any> = []) {
-    const { SUCCESS, WARNING_ID_IS_EMPTY, WARNING_TYPE_IS_EMPTY, WARNING_ACCOUNT_IS_EMPTY, WARNING_NAME_IS_EMPTY, WARNING_PASSWORD_IS_EMPTY, ERROR_REPEAT_NAME, ERROR_REPEAT_ACCOUNT, ERROR_REPEAT_TAG_NUMBER, ERROR_NUKNOWN } = ADD_PORTER_RESPONSE_STATUS;
+    const { SUCCESS, WARNING_ID_IS_EMPTY, WARNING_TYPE_IS_EMPTY, WARNING_ACCOUNT_IS_EMPTY, WARNING_NAME_IS_EMPTY, WARNING_PASSWORD_IS_EMPTY, ERROR_REPEAT_NAME, ERROR_REPEAT_ACCOUNT, ERROR_REPEAT_TAG_NUMBER, ERROR_NUKNOWN, ERROR_TYPE_NOT_FOUND, ERROR_REPEAT_ID } = ADD_PORTER_RESPONSE_STATUS;
 
     let message = '';
 
@@ -33,6 +33,12 @@ export class ResponseHandler {
         break;
       case ERROR_REPEAT_TAG_NUMBER:
         message = '【錯誤：新增傳送員】重複的標籤編號';
+        break;
+      case ERROR_TYPE_NOT_FOUND:
+        message = '【錯誤：新增傳送員】找不到傳送員類型';
+        break;
+      case ERROR_REPEAT_ID:
+        message = '【錯誤：新增傳送員】重複的傳送員編號';
         break;
       case ERROR_NUKNOWN:
         message = '【錯誤：新增傳送員】發生非預期的錯誤';
