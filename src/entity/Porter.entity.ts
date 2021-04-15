8e39bed29bafc56ecea70e8ff74d82ec7c9c1941
Guantou <guantou.lee@gmail.com>
2021-04-15 11:21:35 +0800

    rename file

---
+++ src/entity/Porter.entity.ts
@@ -0,0 +1,50 @@
+import { Column, Entity, Index, ManyToOne, PrimaryColumn } from 'typeorm';
+import { PorterType } from './PorterType.entity';
+
+@Entity('porter_list')
+export class Porter {
+  // 傳送員編號
+  @PrimaryColumn('varchar', {
+    length: 10,
+    name: 'pid'
+  })
+  ID: string;
+
+  // 傳送員姓名
+  @Index({ unique: true })
+  @Column('varchar', {
+    length: 30,
+    name: 'porter_name',
+  })
+  name: string;
+
+  // 傳送員tag標籤編號
+  @Index({ unique: true })
+  @Column('varchar', {
+    length: 25,
+    name: 'tag_number',
+    default: null
+  })
+  tagNumber: string;
+
+  // 傳送員生日日期
+  @Column('date', {
+    name: 'porter_birthday',
+    default: null
+  })
+  birthday: string;
+
+  // 傳送員性別
+  @Column('boolean', {
+    name: 'porter_gender',
+    default: null
+  })
+  gender: boolean;
+
+  // 傳送員類型編號
+  @ManyToOne(
+    () => PorterType,
+    type => type.ID
+  )
+  type: PorterType;
+}
